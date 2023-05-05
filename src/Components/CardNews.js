8de72f5241d7728d920b00dfcee0d0201553f85d
Gabriel Reis <gabrielreis2701@gmail.com>
2023-05-05 17:39:14 -0400

    adição de estilo

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -4,7 +4,7 @@ class CardNews extends HTMLElement{
 
         const shadow = this.attachShadow({mode: "open"})
         shadow.appendChild(this.build());
-        shadow.appendChild(this.styles);
+        shadow.appendChild(this.styles());
 
     }
 
@@ -46,11 +46,48 @@ class CardNews extends HTMLElement{
 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
-        return componentRoot
+        return componentRoot;
     }
 
     styles(){
+        const style = document.createElement("style");
+        style.textContent = `
+        
+        .card{
+            width: 80%;
+            -webkit-box-shadow: 10px 10px 51px 0px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: 10px 10px 51px 0px rgba(0, 0, 0, 0.75);
+            box-shadow: 10px 10px 51px 0px rgba(0, 0, 0, 0.75); 
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+        
+        .card_left{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        .card_left > span {
+            font-weight: 400;
+        }
+        .card_left > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        
+        .card_left > p{
+            color: rgb(70,70,70);
+        }
+        
+        `
 
+
+        return style;
     }
 }
 
